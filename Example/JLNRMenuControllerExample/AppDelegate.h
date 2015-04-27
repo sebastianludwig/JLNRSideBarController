82c1cad0f7e14a73bb2ab691757bfbbdc2c3c01b
Julian Raschke <julian@raschke.de>
2015-04-27 17:34:43 +0200

    Add empty example project

---
+++ Example/JLNRMenuControllerExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JLNRMenuControllerExample
+//
+//  Created by Julian Raschke on 27.04.15.
+//  Copyright (c) 2015 Julian Raschke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Example/JLNRMenuControllerExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JLNRMenuControllerExample
+//
+//  Created by Julian Raschke on 27.04.15.
+//  Copyright (c) 2015 Julian Raschke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
